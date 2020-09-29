4b69f3001b9dd6c7282d6d4798f31dacbd7692d0
김성은 <kse012050@naver.com>
2020-09-29 10:27:42 +0900

    apple clone 해당영역의 스크롤 위치를 0 ~ 1 사이의 값으로 추출하는 변수를 만들었다

--- Interactive/애플 웹사이트 인터랙션 클론!/apple/js/main.js
+++ Interactive/애플 웹사이트 인터랙션 클론!/apple/js/main.js
@@ -7,38 +7,84 @@
 
     const sceneInfo = [
         {
+            //  0
             type: 'sticky',
-            heightNum:5, 
+            heightNum:5,    // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight : 0,
             objs:{
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA:document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB:document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC:document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD:document.querySelector('#scroll-section-0 .main-message.d')
+            },
+            values:{
+                messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+                messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+                messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
+                messageD_opacity_in: [0, 1, { start: 0.7, end: 0.8 }],
+                messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+                messageB_translateY_in: [20, 0, { start: 0.3, end: 0.4 }],
+                messageC_translateY_in: [20, 0, { start: 0.5, end: 0.6 }],
+                messageD_translateY_in: [20, 0, { start: 0.7, end: 0.8 }],
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+                messageB_opacity_out: [1, 0, { start: 0.45, end: 0.5 }],
+                messageC_opacity_out: [1, 0, { start: 0.65, end: 0.7 }],
+                messageD_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
+                messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
+                messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
+                messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
+                messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }]
             }
         },
         {
+            //  1
             type: 'normal',
-            heightNum:0, 
+            // heightNum:5,    // 브라우저 높이의 5배로 scrollHeight 세팅 -> normal에서는 필요없음
             scrollHeight : 0,
             objs:{
                 container: document.querySelector('#scroll-section-1')
             }
         },
         {
+            //  2
             type: 'sticky',
-            heightNum:5, 
+            heightNum:5,    // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight : 0,
             objs:{
-                container: document.querySelector('#scroll-section-2')
+                container: document.querySelector('#scroll-section-2'),
+                messageA: document.querySelector('#scroll-section-2 .a'),
+                messageB: document.querySelector('#scroll-section-2 .b'),
+                messageC: document.querySelector('#scroll-section-2 .c'),
+                pinB: document.querySelector('#scroll-section-2 .b .pin'),
+                pinC: document.querySelector('#scroll-section-2 .c .pin')
+            },
+            values: {
+                messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
+                messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
+                messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
+                messageA_opacity_in: [0, 1, { start: 0.25, end: 0.3 }],
+                messageB_opacity_in: [0, 1, { start: 0.6, end: 0.65 }],
+                messageC_opacity_in: [0, 1, { start: 0.87, end: 0.92 }],
+                messageA_translateY_out: [0, -20, { start: 0.4, end: 0.45 }],
+                messageB_translateY_out: [0, -20, { start: 0.68, end: 0.73 }],
+                messageC_translateY_out: [0, -20, { start: 0.95, end: 1 }],
+                messageA_opacity_out: [1, 0, { start: 0.4, end: 0.45 }],
+                messageB_opacity_out: [1, 0, { start: 0.68, end: 0.73 }],
+                messageC_opacity_out: [1, 0, { start: 0.95, end: 1 }],
+                pinB_scaleY: [0.5, 1, { start: 0.6, end: 0.65 }],
+                pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }]
             }
         },
         {
+            //  3
             type: 'sticky',
-            heightNum:5, 
+            heightNum:5,    // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight : 0,
             objs:{
                 container: document.querySelector('#scroll-section-3')
             }
-        }
-
+        },
     ];
 
     let totalScrollHeight
@@ -66,19 +112,44 @@
 
     };
 
+    function playAnimation(){
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+        switch(currentScene){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            
+        }
+    }
+
     function scrollLoop(){
+        enterNewScene = false;
         prevScrollHeight = 0;
         for(var i = 0; i < currentScene; i++){
             prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
         }
         if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            enterNewScene = true;
             currentScene++;
         }
         if(yOffset < prevScrollHeight){
+            enterNewScene = true;
             if(currentScene === 0) return;
             currentScene--;
         }
         document.body.setAttribute('id', `show-scene-${currentScene}`);
+
+        if(enterNewScene) return;
+        playAnimation();
     }
 
     window.addEventListener('scroll',() => {
