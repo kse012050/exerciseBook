a474722b3086afa8431c4b077b3448445cf65fd8
김성은 <kse012050@naver.com>
2021-02-15 17:23:19 +0900

    bouncingBall 공이 벽에 부딪혔을 때 방향전환을 만들었다

--- Interactive/BouncingBall/js/ball.js
+++ Interactive/BouncingBall/js/ball.js
@@ -4,16 +4,33 @@ export class Ball{
         this.stageHeight = stageHeight;
         this.size = size;
         this.speedX = speed;
-        this.startX = (this.stageWidth / 2) - (this.size);
-        this.startY = (this.stageHeight) - (this.size);
+        this.speedY = speed;
+        this.x = (this.stageWidth / 2) - (this.size);
+        this.y = (this.stageHeight) - (this.size);
     }
 
     draw(ctx){
-        this.startX += this.speedX;
-        console.log(this.speedX);
+        this.x += this.speedX;
+        this.y -= this.speedY;
+        
+        this.bounceWindow(this.stageWidth ,this.stageHeight);
+
         ctx.fillStyle = "yellow";
         ctx.beginPath();
-        ctx.arc(this.startX, this.startY, this.size, 0, 2 * Math.PI);
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
+    }
+
+    bounceWindow(stageWidth, stageHeight){
+        const minX = this.size;
+        const maxX = stageWidth - this.size;
+        const minY = this.size;
+        const maxY = stageHeight - this.size;
+
+        if(this.x > maxX || this.x < minX){
+            this.speedX *= -1;
+        }else if(this.y > maxY || this.y < minY){
+            this.speedY *= -1;
+        }
     }
 }
