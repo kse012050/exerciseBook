f0781b81b0d6a28ae67dfbd30f7bd82dc5fd4978
김성은 <kse012050@naver.com>
2021-02-15 18:35:38 +0900

    scss 준비 완료했다

--- Vue/test/README.md
+++ Vue/test/README.md
@@ -1,4 +1,4 @@
-# test
+# sasstest
 
 ## Project setup
 ```
--- Vue/test/src/App.vue
+++ Vue/test/src/App.vue
@@ -17,13 +17,12 @@ export default {
 </script>
 
 <style lang="scss">
-$color : red;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color:$color;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
