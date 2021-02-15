4b761e3e39b1b6b3513a88a53f90769b28bc96fe
김성은 <kse012050@naver.com>
2021-02-15 16:45:57 +0900

    BouncingBall ball을 만들었다

--- Interactive/BouncingBall/js/app.js
+++ Interactive/BouncingBall/js/app.js
@@ -1,4 +1,5 @@
-import { Block } from './block.js'
+import { Block } from './block.js';
+import { Ball } from './ball.js';
 
 class App{
     constructor(){
@@ -12,19 +13,29 @@ class App{
         this.stageHeight = document.body.clientHeight;
         
         this.block = new Block(this.stageWidth, 10);
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 10 , 10);
         this.resize();
         this.animate();
+
+        // window.requestAnimationFrame(this.animate.bind(this));
     }
 
     resize(){
+        this.canvas.width = this.stageWidth;
+        this.canvas.height = this.stageHeight;
+
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2,2)
-
     }
 
     animate(t){
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0,0,this.stageWidth, this.stageHeight);
+
         this.block.draw(this.ctx);
+        this.ball.draw(this.ctx);
     }
 }
 
--- Interactive/BouncingBall/js/ball.js
+++ Interactive/BouncingBall/js/ball.js
@@ -1,9 +1,19 @@
 export class Ball{
-    constructor(){
-        
+    constructor(stageWidth, stageHeight, size , speed){
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+        this.size = size;
+        this.speedX = speed;
+        this.startX = (this.stageWidth / 2) - (this.size);
+        this.startY = (this.stageHeight) - (this.size);
     }
 
-    draw(){
-
+    draw(ctx){
+        this.startX += this.speedX;
+        console.log(this.speedX);
+        ctx.fillStyle = "yellow";
+        ctx.beginPath();
+        ctx.arc(this.startX, this.startY, this.size, 0, 2 * Math.PI);
+        ctx.fill();
     }
 }
