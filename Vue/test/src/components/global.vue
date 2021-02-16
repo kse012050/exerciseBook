c347e11293461139a93fb72b2c4ea4e53b4d8719
김성은 <kse012050@naver.com>
2021-02-16 18:20:35 +0900

    vue router link 걸기 시도 중..

---
+++ Vue/test/src/components/global.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="globalArea">
+        <header>
+            <h1>
+                <!-- <router-link to="">
+                    <img src="" alt="">
+                </router-link> -->
+            </h1>
+            <h2>HOME</h2>
+            <button>CONTACT</button>
+        </header>
+
+        <aside>
+            <button>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <!-- <router-link to="">HOME</router-link> -->
+                    </li>
+                    <li>
+                        <!-- <router-link to="">BRAND</router-link> -->
+                    </li>
+                    <li>
+                        <!-- <router-link to="">CHANNEL</router-link> -->
+                    </li>
+                    <li>
+                        <!-- <router-link to="">MAGAZINES</router-link> -->
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
