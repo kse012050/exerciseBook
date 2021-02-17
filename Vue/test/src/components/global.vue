0398ff1c4947025c7fdb2050a2633785f08bb29f
김성은 <kse012050@naver.com>
2021-02-17 18:33:54 +0900

    global 요소 css 작업중(scss 생각할게 많다..)

--- Vue/test/src/components/global.vue
+++ Vue/test/src/components/global.vue
@@ -35,7 +35,15 @@
         </aside>
 
         <footer>
-
+            <p>COPYRIGHT © 2018 NATIONAL GEOGRAPHIC KOREA LLT. ALL RIGHT RESERVED</p>
+            <div>
+                <button>개인정보 취급방침 이용약관</button>
+                <ul>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                </ul>
+            </div>
         </footer>
     </div>
 </template>
@@ -47,5 +55,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$pointColor:#fcd804;
+.globalArea{
+    @mixin fixed($width,$left,$test){
+        position: fixed;
+        width: $width;
+        left: $left;
+        @if($test == 1){
+            padding: 0 70px;
+            box-sizing: border-box;
+        }
+        
+    }
+    header{/* position: fixed; top: 55px; left: 0; width: 100%; padding:0 70px; box-sizing: border-box; */
+        @include fixed(100%,0,1); top: 55px;
+        display: flex; align-items: center; 
+        h1{max-width: 194px; padding-right: 30px; border-right: 2px solid $pointColor; margin-right: 40px;
+            img{width: 100%;}
+        }
+        h2{font-size: 30px;}
+        button{margin-left: auto; font-size: 25px; background-color: transparent; border: none; color: $pointColor; padding-right: 0;}
+    }
 
+    footer{
+        @include fixed(100%,0,1); bottom: 0px;
+        display: flex; justify-content: space-between; align-items: center;
+
+        &>*{
+            flex: 1;
+        }
+
+        div{display: flex; justify-content: space-around;}
+    }
+}
 </style>
