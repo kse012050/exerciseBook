bf6b1438e26d4332dec98130b0f7e63877e8efc0
김성은 <kse012050@naver.com>
2021-02-18 17:47:00 +0900

    퍼블리싱 글로벌 요소를 완성했다

--- Vue/test/src/components/global.vue
+++ Vue/test/src/components/global.vue
@@ -39,9 +39,9 @@
             <div>
                 <button>개인정보 취급방침 이용약관</button>
                 <ul>
-                    <li><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
+                    <li><a href="#"><img src="../images/sns_book.png" alt=""></a></li>
+                    <li><a href="#"><img src="../images/sns_instar.png" alt=""></a></li>
+                    <li><a href="#"><img src="../images/sns_youtube.png" alt=""></a></li>
                 </ul>
             </div>
         </footer>
@@ -57,35 +57,42 @@ export default {
 <style lang="scss" scoped>
 $pointColor:#fcd804;
 .globalArea{
-    @mixin fixed($width,$left,$test){
-        position: fixed;
-        width: $width;
-        left: $left;
-        @if($test == 1){
-            padding: 0 70px;
-            box-sizing: border-box;
-        }
-        
-    }
-    header{/* position: fixed; top: 55px; left: 0; width: 100%; padding:0 70px; box-sizing: border-box; */
-        @include fixed(100%,0,1); top: 55px;
+ 
+    header{position: fixed; top: 55px; left: 0; width: 100%; padding:0 70px; box-sizing: border-box;
         display: flex; align-items: center; 
         h1{max-width: 194px; padding-right: 30px; border-right: 2px solid $pointColor; margin-right: 40px;
             img{width: 100%;}
         }
         h2{font-size: 30px;}
-        button{margin-left: auto; font-size: 25px; background-color: transparent; border: none; color: $pointColor; padding-right: 0;}
+        button{margin-left: auto; font-size: 25px; color: $pointColor; padding-right: 0;}
+    }
+
+    aside{
+        button{
+            position: fixed; top: calc(50% - 4px); left: 70px;
+            span{display: inline-block; width: 8px; height: 8px; background-color: white; border-radius: 100%;
+                &:not(:last-of-type){margin-right: 5px;}
+            }
+        }
+        nav{display: none}
     }
 
     footer{
-        @include fixed(100%,0,1); bottom: 0px;
+        position: fixed; left: 0; bottom: 0; width: 100%; 
         display: flex; justify-content: space-between; align-items: center;
-
-        &>*{
-            flex: 1;
+        padding: 45px 0; border-top: 1px solid rgba(255,255,255,0.4);
+        >*{
+            flex: 1; text-align: center;
         }
+        p{font-size: 18px;}
 
-        div{display: flex; justify-content: space-around;}
+        div{display: flex; justify-content: space-around;
+            button{font-size: 18px;}
+            ul{display: flex; justify-content: center;
+
+                li:not(last-of-type){margin-right: 20px;}
+            }
+        }
     }
 }
 </style>
