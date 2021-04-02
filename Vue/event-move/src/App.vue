b08f2e186a4d559f7a48778d85c1fe6b81feb1c0
김성은 <kse012050@naver.com>
2021-04-02 17:26:30 +0900

    vue 이벤트 전달 연습중이다

---
+++ Vue/event-move/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Result></Result>
+    <Btn></Btn>
+  </div>
+</template>
+
+<script>
+import Result from './components/result.vue'
+import Btn from './components/btn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Result,
+    Btn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/event-move/src/components/result.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <strong>결과값</strong>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
