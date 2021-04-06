d078c747d943662a714cd7be6b59525b409bed54
김성은 <kse012050@naver.com>
2021-04-06 17:13:56 +0900

    웨이브 - totalWaves를 만들었다

--- Interactive/움직이는 웨이브 만들기/waveGroup.js
+++ Interactive/움직이는 웨이브 만들기/waveGroup.js
@@ -2,12 +2,13 @@ import { Wave } from './wave.js';
 
 export class WaveGroup{
     constructor(){
+        this.totalWaves = 1;
         this.totalPoints = 6;
         this.waves = [];
-        this.wave = new Wave(1,this.totalPoints);
-        for(let i = 0; i < this.totalPoints; i++){
-            // const wave = new Wave(i , this.totalPoints);
-            // this.waves[i] = wave;
+        // this.wave = new Wave(1,this.totalPoints);
+        for(let i = 0; i < this.totalWaves; i++){
+            const wave = new Wave(i , this.totalPoints);
+            this.waves[i] = wave;
             
         }
     }
@@ -15,16 +16,16 @@ export class WaveGroup{
     resize(stageWidth,stageHeight){
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
-        /* for(let i = 0; i < this.totalPoints; i++){
+        for(let i = 0; i < this.totalWaves; i++){
             this.waves[i].resize(stageWidth,stageHeight);
-        } */
-        this.wave.resize(stageWidth,stageHeight);
+        }
+        // this.wave.resize(stageWidth,stageHeight);
     }
 
     draw(ctx){
-        this.wave.draw(ctx);
-        for(let i = 0; i < this.totalPoints; i++){
-            // this.waves[i].draw(ctx);
+        // this.wave.draw(ctx);
+        for(let i = 0; i < this.totalWaves; i++){
+            this.waves[i].draw(ctx);
             
         }
     }
