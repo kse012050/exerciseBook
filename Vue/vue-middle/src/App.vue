88729608f30d429c57887158049fb9cc94c49938
김성은 <kse012050@naver.com>
2021-12-21 13:47:20 +0900

    ES6 적용했다

--- Vue/vue-middle/src/App.vue
+++ Vue/vue-middle/src/App.vue
@@ -18,34 +18,34 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-    data : function(){
+    data(){
         return {
             todoItems : []
         }
     },
     methods : {
-        addOneItem : function(todoItem){
+        addOneItem(todoItem){
             const obj = {completed : false, item : todoItem}
             localStorage.setItem(todoItem , JSON.stringify(obj));
             this.todoItems.push(obj);
         },
-        removeOneItem : function(todoItem , index){
+        removeOneItem(todoItem , index){
             localStorage.removeItem(todoItem.item);
             this.todoItems.splice(index,1);
         },
-        toggleOneComplete: function(todoItem,index){
+        toggleOneComplete(todoItem,index){
             // todoItem.completed = !todoItem.completed;
             this.todoItems[index].completed = !this.todoItems[index].completed;
             localStorage.removeItem(localStorage.key(index));
             localStorage.setItem(todoItem.item , JSON.stringify(todoItem))
         },
-        clearAllItem: function(){
+        clearAllItem(){
             localStorage.clear();
             this.todoItems = [];
             // this.todoItems.splice(0,this.todoItems.length);
         }
     },
-    created : function(){
+    created(){
         if(localStorage.length > 0){
             for(let i = 0; i < localStorage.length; i++){
                 // console.log(localStorage.key(i));
--- Vue/vue-middle/src/components/TodoInput.vue
+++ Vue/vue-middle/src/components/TodoInput.vue
@@ -20,14 +20,14 @@
 import Modal from './common/Modal.vue';
 
 export default {
-    data: function(){
+    data(){
         return {
             newTodoItem : "",
             showModal : false
         }
     },
     methods : {
-        addTodo : function(){
+        addTodo(){
             if(this.newTodoItem !== ''){
                 this.$emit('addTodoItem',this.newTodoItem)
                 this.clearInput();
@@ -35,12 +35,12 @@ export default {
                 this.showModal = !this.showModal;
             }
         },
-        clearInput : function(){
+        clearInput(){
             this.newTodoItem = "";
         }
     },
     components: {
-        Modal : Modal
+        Modal
     }
 }
 </script>
