01e68e3e3792bd31c1d362fdebfebffbd6472d65
김성은 <kse012050@naver.com>
2022-01-02 23:58:35 +0900

    vue 프로젝트 버전 낮춰서 다시 설정했다

--- Vue/vue-news/src/App.vue
+++ Vue/vue-news/src/App.vue
@@ -1,27 +1,23 @@
 <template>
     <div id="app">
-        hello
+        
     </div>
 </template>
 
 <script>
 
 export default {
-    methods : {
-        fetchData() {
-            console.log('hello');
-        }
-    }
+ 
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
