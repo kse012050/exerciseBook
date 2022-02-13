a2bcd02d7b2ab933b3e422cffba6676dcd84d602
김성은 <kse012050@naver.com>
2022-02-14 03:37:21 +0900

    vue axios api 함수 구조화 방법 및 실습를 진행 중이다 ( itme부터 진행하면 된다 )

--- Vue/vue-news/src/views/JobsView.vue
+++ Vue/vue-news/src/views/JobsView.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        jobs
+        <div v-for="job in jobs" v-bind:key="job.id">{{ job.title }}</div>
     </div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index.js'
 export default {
-
+    data() {
+        return{
+            jobs : []
+        }
+    },
+    created(){
+        let vm = this;
+        fetchJobsList()
+            .then(function(response){
+                console.log(response);
+                vm.jobs = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+    }
 }
 </script>
 
--- Vue/vue-news/src/views/NewsView.vue
+++ Vue/vue-news/src/views/NewsView.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <div v-for="user in users" v-bind:key="user">{{ user.title }}</div>
+        <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
+import { fetchNewsList } from '../api/index.js'
 export default {
     data() {
         return{
@@ -14,7 +15,8 @@ export default {
     },
     created() {
         let vm = this;
-        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        // axios.get('https://api.hnpwa.com/v0/news/1.json')
+        fetchNewsList()
             .then(function(response){
                 console.log(response);
                 vm.users = response.data;
