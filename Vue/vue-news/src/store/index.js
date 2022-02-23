0e0ecfa41945e5bed632ab88c557e615c8735b4f
김성은 <kse012050@naver.com>
2022-02-24 02:42:59 +0900

    ask에 store 적용 했다

--- Vue/vue-news/src/store/index.js
+++ Vue/vue-news/src/store/index.js
@@ -1,20 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList , fetchJobsList } from '../api/index.js'
+import { fetchNewsList , fetchJobsList , fetchAskList } from '../api/index.js'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state : {
         news : [],
-        jobs : []
+        jobs : [],
+        ask : []
     },
     mutations : {
         SET_NEWS(state , news){
             state.news = news;
         },
         SET_JOBS(state , jobs){
-            state.jobs = jobs
+            state.jobs = jobs;
+        },
+        SET_ASK(state , ask){
+            state.ask = ask;
         }
     },
     actions : {
@@ -35,13 +39,22 @@ export const store = new Vuex.Store({
         },
         FETCH_JOBS(context){
             fetchJobsList()
-            .then(response =>{
-                console.log(response);
-                context.commit('SET_JOBS' , response.data)
-            })
-            .catch(function(error){
-                console.log(error);
-            });
+                .then(response =>{
+                    console.log(response);
+                    context.commit('SET_JOBS' , response.data)
+                })
+                .catch(function(error){
+                    console.log(error);
+                });
+        },
+        FETCH_ASK(context){
+            fetchAskList()
+                .then(function(response){
+                    context.commit('SET_ASK', response.data)
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
         }
     }
 })
--- Vue/vue-news/src/views/AskView.vue
+++ Vue/vue-news/src/views/AskView.vue
@@ -1,26 +1,13 @@
 <template>
     <div>
-        <div v-for="ask in ask" v-bind:key="ask.id">{{ ask.title }}</div>
+        <div v-for="ask in this.$store.state.ask" v-bind:key="ask.id">{{ ask.title }}</div>
     </div>
 </template>
 
 <script>
-import { fetchAskList } from "../api/index.js";
 export default {
-    data(){
-        return {
-            ask : []
-        }
-    },
     created(){
-        let vm = this;
-        fetchAskList()
-            .then(function(responsive){
-                vm.ask = responsive.data
-            })
-            .catch(function(error){
-                console.log(error);
-            })
+        this.$store.dispatch('FETCH_ASK')
     }
 }
 </script>
