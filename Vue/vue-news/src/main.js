a8bea882fa2246b0567b6276cae67a232fea4c14
김성은 <kse012050@naver.com>
2022-03-22 12:14:48 +0900

    itemView 진행했다 오랜만에하니까 잘 모르겠다 프로젝트를 하자

--- Vue/vue-news/src/main.js
+++ Vue/vue-news/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes/index.js'
 import { store } from './store/index.js'
+import { css } from './common.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,
-  store
+  store,
+  css
 }).$mount('#app')
