94a8fc3a4bc441716122491139e895ea2c88a14c
김성은 <kse012050@naver.com>
2022-03-25 17:07:33 +0900

    react youtube api 연결 까지 확인했다

--- React/youtube/src/app.jsx
+++ React/youtube/src/app.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
+function App() {
+  const [videos, setVideos] = useState([]);
+  // const [name , setName] = useState('KSE');
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
+    fetch("./videoList_Me.json", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, [])
+  
+  
 
-
-function app() {
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };  
-
-  fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
-    .then(response => console.log(response))
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  return <h1>Hello :)</h1>
+  return <VideoList videos={videos}/>
 }
 
-export default app;
+export default App;
