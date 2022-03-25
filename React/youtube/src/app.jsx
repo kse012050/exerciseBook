09892c4c2f52ff301f29c151ed165ca4827c2824
김성은 <kse012050@naver.com>
2022-03-25 18:43:50 +0900

    json 파일로 연결하기 시도 중..

--- React/youtube/src/app.jsx
+++ React/youtube/src/app.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
 import VideoList from './components/video_list/video_list';
+import test from './videoList_Me';
 
 function App() {
   const [videos, setVideos] = useState([]);
   // const [name , setName] = useState('KSE');
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    // };
     
     // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
-    fetch("./videoList_Me.json", requestOptions)
+    /* fetch(test)
       .then(response => response.json())
       .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+      .catch(error => console.log('error', error)); */
+      setVideos(test.item)
   }, [])
   
   
