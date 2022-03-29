7c0d00461c304e6da7fb05bd677e94de8b80b261
김성은 <kse012050@naver.com>
2022-03-29 17:20:40 +0900

    react 공부 중..

--- React/youtube/src/app.jsx
+++ React/youtube/src/app.jsx
@@ -5,20 +5,38 @@ import test from './videoList_Me';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  // console.log(videos);
+  // console.log(setVideos);
   // const [name , setName] = useState('KSE');
   useEffect(() => {
-    // const requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow'
-    // };
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
     
     // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
-    /* fetch(test)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error)); */
-      setVideos(test.item)
-  }, [])
+    fetch('./videoList_Me', requestOptions)
+      .then(response => {
+        console.log(response);
+        return response.json()
+      })
+      .then(result => {
+        console.log(result);
+        return setVideos(result.items)
+      })
+      .catch(error => console.log('error', error));
+    
+      // setVideos(test.item)
+  }, []);
+  // const requestOptions = {
+  //   method: 'GET',
+  //   redirect: 'follow'
+  // };
+  // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
+  //   // fetch(test)
+  //     .then(response => response.json())
+  //     .then(result => setVideos(result.items))
+  //     .catch(error => console.log('error', error));
   
   
 
--- React/youtube/src/components/video_list/video_list.jsx
+++ React/youtube/src/components/video_list/video_list.jsx
@@ -3,7 +3,7 @@ import VideoItem from '../video_item/video_item';
 
 const VideoList = props => (
             <ul>
-                { props.videos.map(video => ( <VideoItem key={video.id} video={video}/>)) }
+                { props.videos.map(video => ( <VideoItem key={video.id} video={video}/>))}
             </ul>
     );
 
