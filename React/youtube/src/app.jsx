783658d89cbb3b26bcdfa56df6900a99c86dd4de
김성은 <kse012050@naver.com>
2022-03-29 18:11:40 +0900

    json 파일로 업로드 성공, 에러는.... 모르겠다..

--- React/youtube/src/app.jsx
+++ React/youtube/src/app.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
 import VideoList from './components/video_list/video_list';
-import test from './videoList_Me';
+import test from './videoList';
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -14,19 +14,20 @@ function App() {
       redirect: 'follow'
     };
     
-    // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
-    fetch('./videoList_Me', requestOptions)
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", requestOptions)
+    // fetch(test, requestOptions)
       .then(response => {
         console.log(response);
-        return response.json()
+        return response.json();
       })
       .then(result => {
         console.log(result);
         return setVideos(result.items)
       })
       .catch(error => console.log('error', error));
-    
-      // setVideos(test.item)
+
+      // console.log(test.items);
+      // setVideos(test.items)
   }, []);
   // const requestOptions = {
   //   method: 'GET',
