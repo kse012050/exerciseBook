ba7ec80c79ad360a1e6679b21f88820e96f4bacd
김성은 <kse012050@naver.com>
2022-03-31 17:23:21 +0900

    vue youtube 제작 시작

---
+++ 노트북/Vue/youtube/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <video-list></video-list>
+  </div>
+</template>
+
+<script>
+import videoList from './components/videoItem.vue'
+export default {
+  components: {
+    videoList
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 노트북/Vue/youtube/src/components/videoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data :{
+        requestOptions : {
+            method: 'GET',
+            redirect: 'follow'
+        }
+    },
+    created(){
+        /* const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        }; */
+        console.log(axios);
+         fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", this.requestOptions)
+            // fetch(test, requestOptions)
+            .then(response => {
+                console.log(response);
+                return response.json();
+            })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => console.log('error', error))
+    },
+    methods : {
+        test : function(){
+            console.log(1);
+        }
+    }   
+}
+</script>
+
+<style>
+
+</style>
