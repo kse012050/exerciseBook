4ac71cf08985e124fac75951a65fee7cbbe8438d
김성은 <kse012050@naver.com>
2022-04-05 10:28:21 +0900

    fetch() 함수를 이용해서 youtube data 를 받아왔다

--- 노트북/Vue/youtube/src/components/videoItem.vue
+++ 노트북/Vue/youtube/src/components/videoItem.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        test
+        <ul>
+            <li v-for="youtubeData in this.youtubeData" v-bind:key="youtubeData.id">
+                <h2>{{ youtubeData.snippet.title}}</h2>
+                <img v-bind:src="youtubeData.snippet.thumbnails.medium.url" alt="video thumbnail">
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -8,13 +13,14 @@
 import axios from 'axios';
 export default {
     data :function() {
-return {
-        requestOptions : {
-            method: 'GET',
-            redirect: 'follow'
-        }
-    };
-},
+    return {
+            requestOptions : {
+                method: 'GET',
+                redirect: 'follow'
+            },
+            youtubeData : []
+        };
+    },
     created(){
         /* const requestOptions = {
             method: 'GET',
@@ -24,17 +30,21 @@ return {
          fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDYt_M1ZCPgn78zXl23y6ZLnP-5LdWyAhM", this.requestOptions)
             // fetch(test, requestOptions)
             .then(response => {
-                console.log(response);
+                // console.log(response);
                 return response.json();
             })
             .then(result => {
-                console.log(result);
+                console.log(result.items);
+                this.youtubeData = result.items;
+                this.test();
             })
             .catch(error => console.log('error', error))
+
     },
     methods : {
         test : function(){
-            console.log(1);
+            console.log('데이터');
+            console.log(this.youtubeData);
         }
     }   
 }
