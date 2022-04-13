6007401ae679f7de3834bcd7fa6f73794ee971e1
김성은 <kse012050@naver.com>
2022-04-13 10:56:44 +0900

    vetur 삭제해 버렸다

---
+++ Vue/youtube/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <video-list></video-list>
+  </div>
+</template>
+
+<script>
+import VideoList from './components/VideoList.vue';
+export default {
+  components: {
+    VideoList
+  },
+  beforeCreate (){
+    console.log(1);
+  }
+}
+</script>
+
+<style>
+
+</style>
