c9f8c20815f5736f3537a6f8583315f0364631da
김성은 <kse012050@naver.com>
2022-04-14 09:47:54 +0900

    vue 강의 다시 시작.. ( 언제까지 해야 이해되고 외워질까 )

---
+++ Vue/vue-news2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods : {
+    fetchData(){
+      console.log('hello');
+    }
+  }
+}
+</script>
+
+<style>
+</style>
