818574355525ccb5980c4bee9cbc73897959f7ec
김성은 <kse012050@naver.com>
2022-04-26 18:16:26 +0900

    데이터 받아와서 반영하기 어렵다..

--- 노트북/React/news/src/components/news_view/news_view.jsx
+++ 노트북/React/news/src/components/news_view/news_view.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const NewsView = (props) => (
-    <div>
-        news
-    </div>      
-);
+const NewsView = (props) => {
+    // let [news , setNews] = useState('test');
+    let test = []
+    
+    useEffect(()=>{
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+            .then((respone)=>{
+                // console.log(respone);
+                // setNews(respone.data);
+                test = respone.data
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        },[])
+        console.log(test);
+    // setNews = '11';
+    // setNews('setNews')
+    return (
+        <div>
+            news {test}
+        </div>
+    )    
+};
 
 export default NewsView;
