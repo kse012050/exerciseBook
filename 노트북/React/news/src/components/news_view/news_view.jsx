a6114ed5a73b9f1a0c54144bb9fc6e0e069f99ca
김성은 <kse012050@naver.com>
2022-04-27 10:40:34 +0900

    axios fetch 차이와 데이터 변환 아직 모르겠다

--- 노트북/React/news/src/components/news_view/news_view.jsx
+++ 노트북/React/news/src/components/news_view/news_view.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useState , useEffect } from 'react';
 import axios from 'axios';
 
 const NewsView = (props) => {
-    // let [news , setNews] = useState('test');
-    let test = []
+    let [news , setNews] = useState([]);
     
     useEffect(()=>{
-        axios.get('https://api.hnpwa.com/v0/news/1.json')
-            .then((respone)=>{
-                // console.log(respone);
-                // setNews(respone.data);
-                test = respone.data
-            })
-            .catch(function(error){
-                console.log(error);
-            });
-        },[])
-        console.log(test);
-    // setNews = '11';
-    // setNews('setNews')
+        // setNews('11');
+        // axios.get('https://api.hnpwa.com/v0/news/1.json')
+        //     .then((respone)=>{
+        //         console.log(respone.json());
+        //         setNews([1,2])
+        //     })
+        //     .catch(function(error){
+        //         console.log(error);
+        //     });
+    //     var config = {
+    //         method: 'get',
+    //         url: 'https://api.hnpwa.com/v0/news/1.json',
+    //         headers: { }
+    //       };
+          
+    //       axios(config)
+    //       .then(function (response) {
+    //         console.log(JSON.stringify(response.data));
+    //         setNews(JSON.stringify(response.data))
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("https://api.hnpwa.com/v0/news/1.json", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    },[])
+        
     return (
         <div>
-            news {test}
+            news {news}
         </div>
     )    
 };
