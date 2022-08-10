060caa6f9a9a15ba8347e750d9058169f078d2ce
김성은 <kse012050@naver.com>
2022-08-10 16:38:59 +0900

    메인페이지 화면 구성했다

--- React/insurance-claim-center/src/App.js
+++ React/insurance-claim-center/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-// import Main from './router/Main.jsx';
+import Main from './router/main/Main.jsx';
 
 // import test from '../images/logo.png'
 
 function App() {
   return (
     <div className="App">
-      {/* <img src={test} alt="왜안돼?"/> */}
-      {/* <img src={'/images/logo.png'} alt="왜안돼?"/> */}
-      {/* <img src={"./images/logo.png"} alt="상품이미지" width="80%"/> */}
-      <img src={require("./img/shoes1.jpg")} alt="상품이미지" width="80%"/>
-      https://bokjiho.medium.com/react-%EB%A6%AC%EC%95%A1%ED%8A%B8%EC%97%90%EC%84%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EA%B2%BD%EB%A1%9C-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0-public-src-%EB%94%94%EB%A0%89%ED%86%A0%EB%A6%AC-%EC%B0%A8%EC%9D%B4-fddb4f455c2a
-      {/* <Main /> */}
+      <Main />
       시작
     </div>
   );
