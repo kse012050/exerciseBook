41487c19c8c0cd051723eba094f0b5eb19ae55f7
김성은 <kse012050@naver.com>
2023-05-12 15:54:03 +0900

    hooks state 정리

---
+++ React/hooks/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import State from './pages/State';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks 개념</h1>
+      <State />
+    </div>
+  );
+}
+
+export default App;
