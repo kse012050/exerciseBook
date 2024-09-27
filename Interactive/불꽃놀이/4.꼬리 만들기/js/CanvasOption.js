491260c4a90a127a4b031dd8740f37ca425976a4
김성은 <kse012050@naver.com>
2024-09-27 16:03:41 +0900

    원 만들기

---
+++ Interactive/불꽃놀이/4.꼬리 만들기/js/CanvasOption.js
@@ -0,0 +1,12 @@
+export default class CanvasOption{
+    constructor(){
+        this.canvas = document.querySelector('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.dpr = window.devicePixelRatio || 1;
+        this.fps = 60;
+        this.interval = 1000 / this.fps;
+        this.canvasWidth = window.innerWidth;
+        this.canvasHeight = window.innerHeight;
+        this.bgColor = '#000000'
+    }
+}
