9b132365e4849548eea5bed16b266a13992a9836
김성은 <kse012050@naver.com>
2025-01-09 17:26:21 +0900

    particle까지 완성

---
+++ Interactive/불꽃놀이/5.스파크 작업/index.js
@@ -0,0 +1,66 @@
+import CanvasOption from './js/CanvasOption.js';
+import Particle from './js/Particle.js';
+import { randomNum, hypotenuse } from './js/utils.js';
+
+class Canvas extends CanvasOption{
+    constructor(){
+        super();
+        this.particles = []
+    }
+
+    init(){
+        this.canvasWidth = innerWidth;
+        this.canvasHeight = innerHeight;
+        this.canvas.width = this.canvasWidth * this.dpr;
+        this.canvas.height = this.canvasHeight * this.dpr;
+        this.canvas.style.width = this.canvasWidth + 'px';
+        this.canvas.style.height = this.canvasHeight + 'px';
+        this.ctx.scale(this.dpr, this.dpr);
+    }
+
+    createPaticle(){
+        const COUNT = 400;
+        const x = randomNum(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+        const y = randomNum(this.canvasHeight * 0.2, this.canvasHeight * 0.8);
+        for(let a = 0; a < COUNT; a++){
+            const r = randomNum(2, 100) * hypotenuse(this.canvasWidth, this.canvasHeight) * 0.00004;
+            const angle = Math.PI / 180 * randomNum(360, 0)
+            const vx = r * Math.cos(angle);
+            const vy = r * Math.sin(angle);
+            const opacity = randomNum(0.7, 0.9);
+            this.particles.push(new Particle(x, y, vx, vy, opacity))
+        }
+    }
+
+    render(){
+        let now, delta;
+        let then = new Date();
+
+        this.createPaticle()
+
+        const frame = () =>{
+            requestAnimationFrame(frame)
+
+            now = new Date();
+            delta = now - then;
+            if(delta < this.interval) return;
+            this.ctx.fillStyle = 'black';
+            this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+            this.particles.forEach((particle)=>{
+                particle.draw();
+                particle.update();
+            })
+
+            then = now - (delta % this.interval)
+        }
+        requestAnimationFrame(frame)
+    }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener('load', () => {
+    canvas.init();
+    canvas.render();
+})
