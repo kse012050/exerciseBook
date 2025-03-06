b8ec5079dbefd8251dd6e79e0d732158330f05a1
김성은 <kse012050@naver.com>
2025-03-07 05:07:05 +0900

    완성.. 근데 뭔가 쫌 부족하다

---
+++ Interactive/불꽃놀이/5.스파크 작업/index.js
@@ -0,0 +1,121 @@
+import CanvasOption from './js/CanvasOption.js'
+import Tail from './js/Tail.js'
+import Particle from './js/Particle.js'
+import Spark from './js/Spark.js'
+import { randomNumber } from './js/utils.js';
+
+class Canvas extends CanvasOption{
+    constructor(){
+        super();
+        this.tails = [];
+        this.particles =[];
+        this.sparks = [];
+    }
+
+    init(){
+        this.canvasWidth = innerWidth;
+        this.canvasHeight = innerHeight;
+        this.canvas.width = this.canvasWidth * this.dpr;
+        this.canvas.height = this.canvasHeight * this.dpr;
+        this.canvas.style.width = this.canvasWidth + 'px';
+        this.canvas.style.height = this.canvasHeight + 'px';
+        this.ctx.scale(this.dpr, this.dpr);
+    }
+
+    createTail(){
+        const x = randomNumber(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+        const vy = this.canvasHeight * randomNumber(0.01, 0.0125) * -1;
+        const color = randomNumber(0, 360)
+        this.tails.push(new Tail(x, vy, color))
+    }
+
+    createParticle(x, y, color){
+        const COUNT = 400;
+        for(let a = 0; a < COUNT; a++){
+            const r = Math.hypot(this.canvasWidth, this.canvasHeight) * 0.0004 * randomNumber(0, 10);
+            const angle = Math.PI / 180 * randomNumber(0, 360);
+            const vx = Math.cos(angle) * r;
+            const vy = Math.sin(angle) * r;
+            const opacity = randomNumber(0.7, 0.9);
+            this.particles.push(new Particle(x, y, vx, vy, opacity, color))
+        }
+    }
+
+    createSpark(x, y){
+        this.sparks.push(new Spark(x, y));
+    }
+    
+    render(){
+        let now, delta;
+        let then = new Date();
+
+        this.createTail();
+
+        // this.createParticle();
+
+        const frame = () => {
+            now = new Date();
+            requestAnimationFrame(frame);
+            delta = now - then;
+            if(this.interval > delta) return;
+            this.ctx.fillStyle = `black`;
+            this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight)
+
+            if(Math.random() < 0.03){
+                this.createTail();
+            }
+
+            this.tails.forEach((tail, idx) => {
+                tail.draw();
+                tail.update();
+
+                for(let i = 0; i < Math.round(-tail.vy * 0.5); i++){
+                    const x = tail.x;
+                    const y = tail.y;
+                    const vx = randomNumber(-5, 5) * 0.1;
+                    const vy = randomNumber(-5, 5) * 0.1;
+                    const opacity = Math.min(-tail.vy, 0.5);
+                    this.sparks.push(new Spark(x, y, vx, vy, opacity, tail.color))
+                }
+
+                if(tail.opacity < 0.25){
+                    this.createParticle(tail.x, tail.y, tail.color);
+                    this.tails.splice(idx, 1);
+                }
+            })
+
+            this.particles.forEach((particle, idx) => {
+                particle.draw();
+                particle.update();
+
+                if(Math.random() < 0.1){
+                    this.sparks.push(new Spark(particle.x, particle.y, 0, 0, 0.3));
+                }
+
+                if(particle.opacity <= 0) this.particles.splice(idx, 1);
+            })
+
+            this.sparks.forEach((spark, idx) => {
+                spark.draw();
+                spark.update();
+
+                if(spark.opacity < 0.3) this.sparks.splice(idx, 1);
+            })
+
+            then = now - (delta % this.interval);
+        }
+        requestAnimationFrame(frame);
+    }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener('load', () => {
+    canvas.init();
+    canvas.render();
+})
+
+window.addEventListener('resize', () => {
+    canvas.init();
+    canvas.render();
+})
