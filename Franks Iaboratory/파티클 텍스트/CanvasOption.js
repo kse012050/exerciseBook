a7ff7624d10dee6ff36a55b70857625604a26fc4
김성은 <kse012050@naver.com>
2025-04-25 16:58:59 +0900

    백업

---
+++ Franks Iaboratory/파티클 텍스트/CanvasOption.js
@@ -0,0 +1,5 @@
+export default class CanvasOption{
+    constructor(seletor){
+        
+    }
+}
--- Franks Iaboratory/파티클 텍스트/index.js
+++ Franks Iaboratory/파티클 텍스트/index.js
@@ -1,3 +1,4 @@
+import { randomBetween } from "./util.js";
 
 
 const canvasText = document.getElementById('canvas-text');
@@ -5,9 +6,13 @@ const ctxText = canvasText.getContext('2d');
 const canvasTextStyle = getComputedStyle(canvasText);
 const canvasTextWidth = canvasTextStyle.width.replace('px', '');
 const canvasTextHeight = canvasTextStyle.height.replace('px', '');
+const dpr = window.devicePixelRatio || 1;
+const interval = 1000 / 60;
 
 canvasText.width = canvasTextWidth;
 canvasText.height = canvasTextHeight;
+canvasText.style.width = canvasTextWidth + 'px';
+canvasText.style.height = canvasTextHeight + 'px';
 
 
 const gradient = ctxText.createLinearGradient(0, 0, canvasTextWidth, canvasTextHeight);
@@ -16,18 +21,26 @@ gradient.addColorStop(0.5, '#3EC3F7')
 gradient.addColorStop(0.65, 'red')
 ctxText.fillStyle = gradient;
 // ctxText.font = '100px';
-ctxText.font = "bold 180px 'Gothic'";
+ctxText.font = "bold 250px 'Jua', system-ui";
 ctxText.textAlign = 'center';
 ctxText.textBaseline = 'middle';
 ctxText.fillText('김성은', canvasTextWidth / 2, canvasTextHeight / 2)
-const gap = 3;
+const gap = 5;
+
+const particles = [];
+const pixels = ctxText.getImageData(0, 0, canvasTextWidth, canvasTextHeight).data;
+ctxText.clearRect(0, 0, canvasTextWidth, canvasTextHeight);
 
 class Particle{
-    constructor(effect, x, y, color){
+    constructor(effect, x, y, colors){
         this.effect = effect;
         this.x = Math.random() * (canvasTextWidth * 2) - (canvasTextWidth / 2);
         this.y = Math.random() * (canvasTextHeight * 2) - (canvasTextHeight / 2);
-        this.color = color;
+        // this.opacity = randomBetween(-1, 0.3);
+        this.opacity = 1;
+        // this.opacityDelta = randomBetween(0.003, 0.006);
+        // this.opacity = 1;
+        this.colors = colors;
         this.originX = x;
         this.originY = y;
         this.size = gap;
@@ -43,11 +56,22 @@ class Particle{
         this.ease = Math.random() * 0.1 + 0.02;
     }
     draw(){
-        this.effect.fillStyle = this.color;
+        this.effect.fillStyle = `rgba(${this.colors.red}, ${this.colors.green}, ${this.colors.blue}, ${this.opacity})`;
         this.effect.fillRect(this.x, this.y, this.size, this.size);
+        // this.effect.ellipse(this.x, this.y, this.size, this.size, Math.PI / 4, 0, Math.PI * 2);
+        // this.effect.fill();
+        // this.effect.fillStyle = `rgba(255, 255, 255, ${1 - this.opacity})`;
+        // this.effect.fillRect(this.x + this.size / 2, this.y + this.size / 2, this.size / 2, this.size / 2);
         // this.effect.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         // this.effect.fill();
         // this.effect.fillRect(this.originX, this.originY, this.size, this.size);
+    }
+    step01(){
+        this.opacity += this.opacityDelta;
+    }
+    step02(){
+        this.x += (this.originX - this.x) * this.ease;
+        this.y += (this.originY - this.y) * this.ease;
     }
     update(){
         // this.dx = this.effect.mouse.x - this.x;
@@ -67,10 +91,6 @@ class Particle{
     }
 }
 
-const particles = [];
-const pixels = ctxText.getImageData(0, 0, canvasTextWidth, canvasTextHeight).data;
-ctxText.clearRect(0, 0, canvasTextWidth, canvasTextHeight);
-
 for(let y = 0; y < canvasTextHeight; y+=gap){
     for(let x = 0; x < canvasTextWidth; x+=gap){
         const index = (y * canvasTextWidth + x) * 4;
@@ -83,23 +103,62 @@ for(let y = 0; y < canvasTextHeight; y+=gap){
             const red = pixels[index];
             const green = pixels[index + 1];
             const blue = pixels[index + 2];
-            const color = `rgba(${red}, ${green}, ${blue}, ${1})`;
-            const particle = new Particle(ctxText, x, y, color);
+            // const color = `rgba(${red}, ${green}, ${blue}, ${1})`;
+            const colors = {
+                red,
+                green,
+                blue,
+            }
+            const particle = new Particle(ctxText, x, y, colors);
             particles.push(particle);
         }
     }
 }
 
-console.log(particles);
+function render(){
+    let now, delta;
+    let then = Date.now();
+    let animateStep = 'step02';
+    
+    const frame = () => {
+        requestAnimationFrame(frame);
 
+        now = Date.now();
+        delta = now - then;
 
-function animate(){
-    ctxText.clearRect(0, 0, canvasTextWidth, canvasTextHeight);
-    particles.forEach((particle) => {
-        particle.update();
-        particle.draw();
-    })
-    requestAnimationFrame(animate);
+        if(delta < interval) return;
+
+        ctxText.clearRect(0, 0, canvasTextWidth, canvasTextHeight);
+        // if(animateStep === 'step01' && particles.some((particle) => particle.opacity >= 1)){
+        //     animateStep = 'step02';
+        // }
+        // if(animateStep === 'step02' && particles.some((particle) => particle.originX === particle.x)){
+        //     animateStep = 'step03';
+        // }
+        particles.forEach((particle) => {
+            particle.draw();
+            particle[animateStep]();
+            // particle.update();
+            // if(particle.opacity < 1){
+            //     particle.step01();
+            // }else{
+            //     particle.step02()
+            // }
+
+            // if(animateStep === 'step03'){
+            //     console.log('?');
+                
+            // }
+            
+        })
+
+        then = now - (delta % interval);
+    }
+    requestAnimationFrame(frame);
 }
-animate();
+
+
+window.addEventListener('load', () => {
+    render();
+})
 
