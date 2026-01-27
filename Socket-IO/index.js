1c600eee8614e37261849694762f9465023c36d5
김성은 <kse012050@naver.com>
2026-01-27 17:53:00 +0900

    이벤트 발생

--- Socket-IO/index.js
+++ Socket-IO/index.js
@@ -11,10 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+// });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
     });
 });
 
