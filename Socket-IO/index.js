cf0f9c1eafc77b9a24d82d6627f5c4df56520628
김성은 <kse012050@naver.com>
2026-01-28 17:22:03 +0900

    방송 - 채팅 기능 완성

--- Socket-IO/index.js
+++ Socket-IO/index.js
@@ -18,10 +18,17 @@ app.get('/', (req, res) => {
 //     });
 // });
 
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+// });
+
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
+  socket.broadcast.emit('hi');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
