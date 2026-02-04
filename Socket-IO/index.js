d92f7fd4f1e08440bba570a283360799a555bccd
김성은 <kse012050@naver.com>
2026-02-04 11:20:47 +0900

    Socket 다시 접속시 이전 메세지 보존,    버튼 disconnect는 왜 안되지?

--- Socket-IO/index.js
+++ Socket-IO/index.js
@@ -5,43 +5,57 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
+
 const io = new Server(server, {
     connectionStateRecovery: {}
 });
 
+// ✅ 메시지 저장소 (메모리)
+const messages = [];
+let messageId = 0;
+
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+    res.sendFile(join(__dirname, 'index.html'));
 });
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-// });
-
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         console.log('message: ' + msg);
-//     });
-// });
 
 io.on('connection', (socket) => {
-  console.log('user connected:', socket.id);
-  socket.broadcast.emit('user connection', socket.id);
 
-  socket.broadcast.emit('hi');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+    console.log('user connected:', socket.id);
 
-  socket.on('disconnect', (reason) => {
-    console.log('user disconnected:', socket.id, reason);
-    socket.broadcast.emit('user disconnected', socket.id);
-  });
+    socket.broadcast.emit('user connection', socket.id);
+    socket.broadcast.emit('hi');
+
+    // ✅ connectionStateRecovery 실패 시 offset 복구
+    if (!socket.recovered) {
+
+        const offset = socket.handshake.auth.offset || 0;
+
+        messages
+            .filter((m) => m.id > offset)
+            .forEach((m) => {
+                socket.emit('chat message', m.text, m.id);
+            });
+    }
+
+    socket.on('chat message', (msg) => {
+        console.log(messages);
+        
+
+        messageId++;
+
+        const message = {
+            id: messageId,
+            text: msg
+        };
+
+        // ✅ 메시지 저장
+        messages.push(message);
+
+        io.emit('chat message', message.text, message.id);
+    });
+
 });
 
-
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log('server running at http://localhost:3000');
 });
