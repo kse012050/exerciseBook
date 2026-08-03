e22bb8217825f46e6906de73e74e23cd8a283435
김성은 <kse012050@naver.com>
2026-08-03 16:05:15 +0900

    파도 만드는 중이다

--- Franks Iaboratory/파티클 텍스트/CanvasOption.js
+++
@@ -1,5 +0,0 @@
-export default class CanvasOption{
-    constructor(seletor){
-        
-    }
-}
--- React/card-maker/src/components/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Test = (props) => (
-    <div>
-        test
-    </div>          
-);
-
-export default Test;
